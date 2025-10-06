e69bb5a31adcffdb2d32ed86fbb38c08e92bf44d
SohZHong <sohzhehong09@gmail.com>
2025-10-06 16:57:02 +0800

    feat: changed structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,12 @@
-// eslint.config.mjs
-import js from '@eslint/js';
-import ts from 'typescript-eslint';
-import nextPlugin from '@next/eslint-plugin-next';
-
 export default [
-  js.configs.recommended,
-  ...ts.configs.recommended,
   {
-    ignores: ['dist/', 'node_modules/', 'packages/**/templates/**'],
+    ignores: ['node_modules', 'dist', 'packages/**/templates/**'],
   },
   {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: { parser: ts.parser },
-    plugins: {
-      '@typescript-eslint': ts.plugin,
-      next: nextPlugin,
-    },
+    files: ['apps/**/*.{ts,tsx,js,jsx}', 'packages/**/*.{ts,tsx,js,jsx}'],
     rules: {
       'no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'next/no-html-link-for-pages': 'off',
     },
   },
 ];
